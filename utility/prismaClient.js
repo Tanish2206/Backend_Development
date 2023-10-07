e4ca7cb87ef385fca505cde73d75604ca062c32e
Tanishjain22 <tanish.daji@gmail.com>
2023-10-07 15:30:51 +0530

    updated client

--- utility/prismaClient.js
+++ utility/prismaClient.js
@@ -2,10 +2,7 @@
 const { PrismaClient } = require('@prisma/client');
 
 // Create an instance of Prisma client
-export const prisma = global.prisma||new PrismaClient({
-    log:['query'],
-})
+const prisma = new PrismaClient();
 
-if (process.env.NODE_ENV!=='production') global.prisma=prisma
-
-module.exports=prisma
+// Export the Prisma client instance
+module.exports = prisma;
